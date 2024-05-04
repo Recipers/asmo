06d368f85978f382a530aeeb378bef72153a754d
Rohsooil <food8123@gmail.com>
2024-05-04 14:07:37 +0900

    initial commit

---
+++ backend/src/main/java/com/recipers/asmo/AsmoApplication.java
@@ -0,0 +1,13 @@
+package com.recipers.asmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/recipers/asmo/AsmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipers.asmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
