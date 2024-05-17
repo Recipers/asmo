a3de7f4c20b06e37a201c4ebe5741785579a1ea3
Rohsooil <food8123@gmail.com>
2024-05-18 00:13:47 +0900

    feat: 유저 엔티티 정의

--- backend/src/main/java/com/recipers/asmo/AsmoApplication.java
+++ backend/src/main/java/com/recipers/asmo/AsmoApplication.java
@@ -1,13 +1,22 @@
 package com.recipers.asmo;
 
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AsmoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AsmoApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(AsmoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 }
---
+++ backend/src/main/java/com/recipers/asmo/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.recipers.asmo.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+}
