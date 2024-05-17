5ae83f22dde3eb48f322d4cabeaa6f51a0357232
minwooKang <rkdalsdn94@gmail.com>
2024-05-18 00:25:03 +0900

    feat: 팀 엔티티 정의

---
+++ backend/src/main/java/com/recipers/asmo/team/entity/Team.java
@@ -0,0 +1,46 @@
+package com.recipers.asmo.team.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "team")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Team {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name ="region", nullable = false)
+    private String region;
+
+    @Column(name = "uniform_color")
+    private String uniformColor;
+
+    @Column(name = "mmr")
+    private int mmr = 1000;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+}
