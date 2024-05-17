92f6b0c95d6d4988ea67981533d2dc6a87339775
Yoonhyeon Jeon <yh.jeon@classting.com>
2024-05-18 00:41:55 +0900

    feat: 팀 멤버 엔티티 정의
    Signed-off-by: Yoonhyeon Jeon <yh.jeon@classting.com>

---
+++ backend/src/main/java/com/recipers/asmo/team_member/entity/TeamMember.java
@@ -0,0 +1,48 @@
+package com.recipers.asmo.team_member.entity;
+
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "team_member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TeamMember {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "team_id", nullable = false)
+    private Long teamId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    // "감독": manager | "코치": coach | "팀원": member
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+}
