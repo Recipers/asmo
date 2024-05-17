97f1e92d8da5980bfb42701f5bf5bd0f6cf32d38
minwooKang <rkdalsdn94@gmail.com>
2024-05-18 05:52:02 +0900

    feat: 팀 생성 API

---
+++ backend/src/main/java/com/recipers/asmo/team/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.recipers.asmo.team.repository;
+
+import com.recipers.asmo.team.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long>{
+}
---
+++ backend/src/main/java/com/recipers/asmo/team/service/TeamService.java
@@ -0,0 +1,21 @@
+package com.recipers.asmo.team.service;
+
+import com.recipers.asmo.team.dto.TeamCreateRequest;
+import com.recipers.asmo.team.entity.Team;
+import com.recipers.asmo.team.mapper.TeamMapper;
+import com.recipers.asmo.team.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TeamService {
+
+    private final TeamMapper teamMapper;
+    private final TeamRepository teamRepository;
+
+    public void createTeam(TeamCreateRequest teamCreateRequest) {
+        Team team = teamMapper.toTeam(teamCreateRequest);
+        teamRepository.save(team);
+    }
+}
