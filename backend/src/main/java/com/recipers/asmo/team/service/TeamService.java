d7f4c2157815ce20f067ee49c33c515a38f165cd
minwooKang <rkdalsdn94@gmail.com>
2024-05-18 14:58:29 +0900

    feat: 팀 조회 API

--- backend/src/main/java/com/recipers/asmo/team/service/TeamService.java
+++ backend/src/main/java/com/recipers/asmo/team/service/TeamService.java
@@ -5,6 +5,8 @@ import com.recipers.asmo.team.entity.Team;
 import com.recipers.asmo.team.mapper.TeamMapper;
 import com.recipers.asmo.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +19,9 @@ public class TeamService {
     public void createTeam(TeamCreateRequest teamCreateRequest) {
         Team team = teamMapper.toTeam(teamCreateRequest);
         teamRepository.save(team);
+    }
+
+    public Page<Team> findAllTeams(Pageable pageable) {
+        return teamRepository.findAll(pageable);
     }
 }
