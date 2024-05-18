048943dc69d1feb1e5019e14519e1a015b5aea58
Yoonhyeon Jeon <yh.jeon@classting.com>
2024-05-18 18:17:09 +0900

    feat: 팀의 전적 목록 조회 API 구현
    Signed-off-by: Yoonhyeon Jeon <yh.jeon@classting.com>

--- backend/src/main/java/com/recipers/asmo/game_history/service/GameHistoryService.java
+++ backend/src/main/java/com/recipers/asmo/game_history/service/GameHistoryService.java
@@ -1,6 +1,8 @@
 package com.recipers.asmo.game_history.service;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.recipers.asmo.game_history.entity.GameHistory;
 import com.recipers.asmo.game_history.repository.GameHistoryRepository;
@@ -14,6 +16,10 @@ public class GameHistoryService {
 
     public Optional<GameHistory> findGameHistory(Long id) {
         return gameHistoryRepository.findById(id);
+    }
+
+    public Page<GameHistory> findGameHistoriesByTeamId(Long teamId, Pageable pageable) {
+        return gameHistoryRepository.findByWinnerTeamIdOrLoserTeamId(teamId, teamId, pageable);
     }
 
 }
