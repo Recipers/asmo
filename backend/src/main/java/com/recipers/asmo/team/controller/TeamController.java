27eaed3532da40607fbedfc02996a9932bf3ae83
minwooKang <rkdalsdn94@gmail.com>
2024-05-18 14:58:29 +0900

    feat: 팀 조회 API

--- backend/src/main/java/com/recipers/asmo/team/controller/TeamController.java
+++ backend/src/main/java/com/recipers/asmo/team/controller/TeamController.java
@@ -1,9 +1,13 @@
 package com.recipers.asmo.team.controller;
 
 import com.recipers.asmo.team.dto.TeamCreateRequest;
+import com.recipers.asmo.team.entity.Team;
 import com.recipers.asmo.team.service.TeamService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,11 @@ import org.springframework.web.bind.annotation.*;
 public class TeamController {
 
     private final TeamService teamService;
+
+    @GetMapping(path = "")
+    public ResponseEntity<Page<Team>> findAllTeams(@PageableDefault(size = 10, page = 0) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(teamService.findAllTeams(pageable));
+    }
 
     @PostMapping(path = "")
     public ResponseEntity<Void> createTeam(@RequestBody @Valid TeamCreateRequest teamCreateRequest) {
