37c90315d3707e5c2bbd5e7de4e9c9a05348973e
Yoonhyeon Jeon <yh.jeon@classting.com>
2024-05-18 18:17:09 +0900

    feat: 팀의 전적 목록 조회 API 구현
    Signed-off-by: Yoonhyeon Jeon <yh.jeon@classting.com>

--- backend/src/main/java/com/recipers/asmo/game_history/controller/GameHistoryController.java
+++ backend/src/main/java/com/recipers/asmo/game_history/controller/GameHistoryController.java
@@ -1,6 +1,9 @@
 package com.recipers.asmo.game_history.controller;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +15,13 @@ import com.recipers.asmo.game_history.service.GameHistoryService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping(path = "/game-histories")
+@RequestMapping(path = "")
 @RequiredArgsConstructor
 public class GameHistoryController {
 
     private final GameHistoryService gameHistoryService;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/game-histories/{id}")
     public ResponseEntity<GameHistory> findGameHistory(@PathVariable("id") Long id) {
         Optional<GameHistory> gameHistoryOpt = gameHistoryService.findGameHistory(id);
 
@@ -29,6 +32,13 @@ public class GameHistoryController {
         GameHistory gameHistory = gameHistoryOpt.get();
 
         return ResponseEntity.status(HttpStatus.OK).body(gameHistory);
+    }
+
+    @GetMapping(path = "/teams/{teamId}/game-histories")
+    public ResponseEntity<Page<GameHistory>> findGameHistoryByTeamId(@PathVariable("teamId") Long teamId,
+        @PageableDefault(size = 10, page = 0) Pageable pageable) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(gameHistoryService.findGameHistoriesByTeamId(teamId, pageable));
     }
 
 }
--- backend/src/main/java/com/recipers/asmo/game_history/repository/GameHistoryRepository.java
+++ backend/src/main/java/com/recipers/asmo/game_history/repository/GameHistoryRepository.java
@@ -1,7 +1,12 @@
 package com.recipers.asmo.game_history.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.recipers.asmo.game_history.entity.GameHistory;
 
 public interface GameHistoryRepository extends JpaRepository<GameHistory, Long>{
+
+    Page<GameHistory> findByWinnerTeamIdOrLoserTeamId(Long winnerTeamId, Long loserTeamId, Pageable pageable);
+
 }
