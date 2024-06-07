6f9004564e56ab17b36da8747a8c2c560a701aaa
Yoonhyeon Jeon <yh.jeon@classting.com>
2024-06-08 02:27:48 +0900

    feat: 게임의 신청 목록 조회 api 구현
    Signed-off-by: Yoonhyeon Jeon <yh.jeon@classting.com>

--- backend/src/main/java/com/recipers/asmo/game_proposal/repository/GameProposalRepository.java
+++ backend/src/main/java/com/recipers/asmo/game_proposal/repository/GameProposalRepository.java
@@ -1,7 +1,10 @@
 package com.recipers.asmo.game_proposal.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.recipers.asmo.game_proposal.entity.GameProposal;
 
 public interface GameProposalRepository extends JpaRepository<GameProposal, Long> {
+    Page<GameProposal> findByGameId(Pageable pageable, Long gameId);
 }
