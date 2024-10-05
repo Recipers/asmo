309a0f26244880c54249a879080bb374e7d1a446
Yoonhyeon Jeon <yh.jeon@classting.com>
2024-10-05 16:14:07 +0900

    feat(front): 캘린더 페이지 구현
    Signed-off-by: Yoonhyeon Jeon <yh.jeon@classting.com>

---
+++ front/src/screens/CalendarScreen.tsx
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+import {colors} from '../constants';
+
+LocaleConfig.locales.ko = {
+  monthNames: [
+    '01월',
+    '02월',
+    '03월',
+    '04월',
+    '05월',
+    '06월',
+    '07월',
+    '08월',
+    '09월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: '오늘',
+};
+LocaleConfig.defaultLocale = 'ko';
+
+const CalendarScreen = () => {
+  const [selected, setSelected] = useState('');
+
+  return (
+    <Calendar
+      monthFormat={'yyyy년 M월'}
+      onDayPress={day => {
+        setSelected(day.dateString);
+      }}
+      markedDates={{
+        [selected]: {
+          selected: true,
+          disableTouchEvent: true,
+          selectedDotColor: 'orange',
+          selectedColor: colors.PURPLE_200,
+        },
+      }}
+      theme={{
+        todayTextColor: colors.PURPLE_200,
+      }}
+    />
+  );
+};
+
+export default CalendarScreen;
